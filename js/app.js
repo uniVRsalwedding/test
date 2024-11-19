bc1d9d55b7b0b5771e99ca95e52b6bed1d0ea9a1
sergio-rg <103499564+sergio-rg@users.noreply.github.com>
2024-11-19 20:24:02 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ function initScene() {
 
             minion = document.createElement('a-entity')
             minion.setAttribute('gltf-model', '#minion' )
-            minion.setAttribute('scale', '2 2 2')
+            minion.setAttribute('scale', '2.5 2.5 2.5')
 
             minion.setAttribute('class', 'minion')
             minion.object3D.position.set(pos.x, pos.y, pos.z)
