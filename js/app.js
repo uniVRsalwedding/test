ffffea761e2af86d1465d005ea7e653b885c16eb
sergio-rg <sergiorg9922@gmail.com>
2024-12-17 17:55:54 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -62,11 +62,11 @@ function llamar_telefono() {
 	
     phone.setAttribute('gltf-model', '#phone')
 			
-	phone.setAttribute('scale', '1 1 1')
+	//phone.setAttribute('scale', '1 1 1')
 	console.log('1')
 	phone.object3D.position.set(0, 0, 0)
 	
-	phone.setAttribute('rotation', '0 180 0')
+	//phone.setAttribute('rotation', '0 180 0')
 	phone.setAttribute('shootablephone', '')
 
 	
