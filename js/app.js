c81bbcff61e39dbe36762e0839d5f145ab2327b2
sergio-rg <sergiorg9922@gmail.com>
2024-12-17 18:56:15 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -104,7 +104,7 @@ AFRAME.registerComponent('shootablewinner', {
 			phone.setAttribute('gltf-model', '#phone')
 					
 			phone.setAttribute('scale', '1 1 1')
-			phone.object3D.position.set(0, 0, 3)
+			phone.object3D.position.set(0, 0, 1)
 			
 			//phone.setAttribute('rotation', '0 180 0')
 			phone.setAttribute('shootablephone', '')
