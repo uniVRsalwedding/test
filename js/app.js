71441790aa5740c73540174e8e9adf51ac4f3631
sergio-rg <sergiorg9922@gmail.com>
2024-12-31 15:13:33 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -84,14 +84,15 @@ AFRAME.registerComponent('shootablewinner', {
 			vid.setAttribute('src','#video');
 			vid.setAttribute('width', '4');
             vid.setAttribute('height', '6');
-            vid.setAttribute('position', '-5 3 10');  
+            vid.setAttribute('position', '-3 3 10');
+			vid.setAttribute('rotation', '0 30 0');
 			vid.setAttribute('shootablevideo','');
 			vid.setAttribute('class','minion');
 			
 			web = document.createElement('a-entity');
 			web.setAttribute('data-raycastable','');
 			web.setAttribute('gltf-model','#web');
-			web.setAttribute('position','-5 -3 10');
+			web.setAttribute('position','-3 -3 10');
 			web.setAttribute('scale','0.5 0.5 0.5');
 			web.setAttribute('rotation','0 90 0');
 			web.setAttribute('class','clickable remove minion');
@@ -104,14 +105,15 @@ AFRAME.registerComponent('shootablewinner', {
 			vid2.setAttribute('src','#video');
 			vid2.setAttribute('width', '4');
             vid2.setAttribute('height', '6');
-            vid2.setAttribute('position', '-15 3 10');  
+            vid2.setAttribute('position', '-35 3 10');
+			vid2.setAttribute('rotation', '0 120 0');
 			vid2.setAttribute('shootablevideo','');
 			vid2.setAttribute('class','minion');
 			
 			web2 = document.createElement('a-entity');
 			web2.setAttribute('data-raycastable','');
 			web2.setAttribute('gltf-model','#web');
-			web2.setAttribute('position','-15 -3 10');
+			web2.setAttribute('position','-35 -3 10');
 			web2.setAttribute('scale','0.5 0.5 0.5');
 			web2.setAttribute('rotation','0 90 0');
 			web2.setAttribute('class','clickable remove minion');
