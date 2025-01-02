b9629d9849a8afe859785df596d91cf56f8c2d34
sergio-rg <sergiorg9922@gmail.com>
2025-01-02 14:09:31 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -105,7 +105,7 @@ AFRAME.registerComponent('shootablewinner', {
 			vid2.setAttribute('src','#video');
 			vid2.setAttribute('width', '120');
             vid2.setAttribute('height', '150');
-            vid2.setAttribute('position', '-500 80 0');
+            vid2.setAttribute('position', '-700 80 0');
 			vid2.setAttribute('rotation', '0 -75 0');
 			vid2.setAttribute('shootablevideo','');
 			vid2.setAttribute('class','minion');
@@ -123,7 +123,7 @@ AFRAME.registerComponent('shootablewinner', {
 			
 			document.querySelector('a-scene').appendChild(vid);
 			document.querySelector('a-scene').appendChild(web);
-			document.querySelector('a-scene').appendChild(vid2);
+			document.querySelector('a-video').appendChild(vid2);
 			document.querySelector('a-scene').appendChild(web2);
 		
 
