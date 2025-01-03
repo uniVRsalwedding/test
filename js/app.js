7bdbcfa82d51b8ff27a6402a587bcc388d41c5cf
sergio-rg <sergiorg9922@gmail.com>
2025-01-03 09:56:11 +0100

    escala boton

--- js/app.js
+++ js/app.js
@@ -93,7 +93,8 @@ AFRAME.registerComponent('shootablewinner', {
 			web = document.createElement('a-entity');
 			web.setAttribute('data-raycastable','');
 			web.setAttribute('gltf-model','#web');
-			web.setAttribute('position','0 0 50');
+			web.setAttribute('position','0 0 0');
+			web.setAttribute('scale', '0.5 0.5 0.5');
 			web.setAttribute('rotation','0 90 0');
 			web.setAttribute('class','clickable remove minion');
 			web.addEventListener('click', () => {
