0fc8124e8ac858ee80fa547e8df3218f01cfbb9d
sergio-rg <sergiorg9922@gmail.com>
2025-01-03 10:55:37 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -93,8 +93,8 @@ AFRAME.registerComponent('shootablewinner', {
 			web = document.createElement('a-entity');
 			web.setAttribute('data-raycastable','');
 			web.setAttribute('gltf-model','#web');
-			web.setAttribute('position','0 -5 30');
-			web.setAttribute('rotation','0 90 -10');
+			web.setAttribute('position','5 5 30');
+			web.setAttribute('rotation','0 90 -15');
 			web.setAttribute('class','clickable remove minion');
 			web.addEventListener('click', () => {
 				window.location.href = 'https://planning.wedding/website/zi6cr1i3g6';
