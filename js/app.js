3478b43297711e16fb459e5307598a1c9056341c
sergio-rg <sergiorg9922@gmail.com>
2025-01-06 18:50:18 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -124,39 +124,17 @@ AFRAME.registerComponent('shootablewinner', {
 
 AFRAME.registerComponent('shootablevideo', {
     init: function () {
-		this.firstClick = true;
 		const videoEl = document.querySelector('#video');
 		
-        this.el.addEventListener('click', () => {
-/*
-			if (firstClick) {
-				videoEl.play();
-				firstClick = false;
-				
-				web = document.createElement('a-entity');
-				web.setAttribute('data-raycastable','');
-				web.setAttribute('gltf-model','#web');
-				web.setAttribute('position','8 0 30');
-				web.setAttribute('scale', '1.5 1.5 1.5');
-				web.setAttribute('rotation','0 95 0');
-				web.setAttribute('class','clickable remove minion');
-				web.addEventListener('click', () => {
-					window.location.href = 'https://planning.wedding/website/zi6cr1i3g6';
-				});
-				document.querySelector('a-scene').appendChild(web);
-			}
+		videoEl.addEventListener('ended', crearWeb);
 		
-			else {*/
+        this.el.addEventListener('click', () => {
+
 				if (videoEl.paused) {
 					videoEl.play();
-					if (this.firstClick) {
-						crearWeb();
-						this.firstClick = false;
-					}
 				} else {
 					videoEl.pause();
 				}
-			//}
 		});
       }
 	  
