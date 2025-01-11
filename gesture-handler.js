1b6670b62bc31836041ad899414bd6cbf9852884
sergio-rg <sergiorg9922@gmail.com>
2025-01-11 19:42:31 +0100

    test

--- gesture-handler.js
+++ gesture-handler.js
@@ -67,7 +67,6 @@ window.onload = function () {
 		
 		    // Remove existing camera
 			const oldCamera = document.querySelector('[camera]');
-			oldCamera.setAttribute('position','0 0 0'); /*
 			oldCamera.parentNode.removeChild(oldCamera);
 			
 			// Create new camera with exact same setup as index2.html
@@ -84,7 +83,7 @@ window.onload = function () {
 			cursor.setAttribute('material', 'shader: flat');
 			
 			newCamera.appendChild(cursor);
-			scene.appendChild(newCamera);*/
+			scene.appendChild(newCamera);
 	
 		sky = document.createElement('a-sky')
 		sky.setAttribute('rotation', '0 30 0')
