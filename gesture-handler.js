b30122c74a762830da48adfaaf613e621903bf10
sergio-rg <sergiorg9922@gmail.com>
2025-01-11 18:01:29 +0100

    Update gesture-handler.js

--- gesture-handler.js
+++ gesture-handler.js
@@ -215,8 +215,20 @@ function createOrbit(position, rotation, animationProps) {
   
   // Añade la entidad a la escena
   scene.appendChild(entity);
-  
-  
+}
+
+function crearWeb() {
+	const web = document.createElement('a-entity');
+	web.setAttribute('data-raycastable','');
+	web.setAttribute('gltf-model','#web');
+	web.setAttribute('position','8 0 30');
+	web.setAttribute('scale', '1.5 1.5 1.5');
+	web.setAttribute('rotation','0 95 0');
+	web.setAttribute('class','clickable remove minion');
+	web.addEventListener('click', () => {
+		window.location.href = 'https://planning.wedding/website/zi6cr1i3g6';
+	});
+	document.querySelector('a-scene').appendChild(web);
 }
 
       // Reproducir audio y actualizar estado al hacer clic en Bowser
@@ -253,6 +265,75 @@ function createOrbit(position, rotation, animationProps) {
 
 
 /* global AFRAME, THREE */
+
+AFRAME.registerComponent('shootable', {
+    init: function () {
+        this.el.addEventListener('click', () => {
+            this.el.parentNode.removeChild(this.el)
+            document.getElementById("audio_lengua").play();
+        })
+    }
+});
+
+AFRAME.registerComponent('shootablewinner', {
+    init: function () {
+        this.el.addEventListener('click', () => {
+            // Seleccionar todas las entidades de las clases .orbit y .minion
+            document.querySelectorAll('.orbit, .minion').forEach(entity => {
+                entity.parentNode.removeChild(entity);
+            });
+            document.getElementById("audio_banana").play();
+			
+			const sky = document.querySelector('a-sky');
+			
+            sky.setAttribute('src','#sky2');
+			sky.setAttribute('rotation','0 100 0');
+			
+			vid = document.createElement('a-video');
+			vid.setAttribute('src','#video');
+			vid.setAttribute('width', '14');
+            vid.setAttribute('height', '17');
+            vid.setAttribute('position', '-25 9 30');
+			vid.setAttribute('rotation', '0 -40 0');
+			vid.setAttribute('shootablevideo','');
+			vid.setAttribute('class','minion');
+			
+			
+			vid2 = document.createElement('a-video');
+			vid2.setAttribute('src','#video');
+			vid2.setAttribute('width', '25');
+            vid2.setAttribute('height', '28');
+            vid2.setAttribute('position', '59 16 30');
+			vid2.setAttribute('rotation', '0 53 0');
+			vid2.setAttribute('shootablevideo','');
+			vid2.setAttribute('class','minion');
+			
+			
+			document.querySelector('a-scene').appendChild(vid);
+			document.querySelector('a-scene').appendChild(vid2);
+        });
+    }
+});
+
+
+
+AFRAME.registerComponent('shootablevideo', {
+    init: function () {
+		const videoEl = document.querySelector('#video');
+		
+		videoEl.addEventListener('ended', crearWeb);
+		
+        this.el.addEventListener('click', () => {
+
+				if (videoEl.paused) {
+					videoEl.play();
+				} else {
+					videoEl.pause();
+				}
+		});
+      }
+	  
+});
 
 AFRAME.registerComponent("gesture-handler", {
   schema: {
@@ -320,37 +401,4 @@ AFRAME.registerComponent("gesture-handler", {
   },
 });
 
-/*
-// Componente 1: Panel de texto
-      AFRAME.registerComponent('text-panel', {
-        init: function () {
-          let scene = this.el.object3D;
-
-          // Crear un rectángulo (plano)
-          const geometry = new THREE.PlaneGeometry(2, 1);
-          
-          // Crear un lienzo para el texto
-          const canvas = document.createElement('canvas');
-          canvas.width = 512;
-          canvas.height = 256;
-          const context = canvas.getContext('2d');
-
-          // Configurar el texto en el lienzo
-          context.fillStyle = ' #fffb00';  // Color de fondo
-          context.fillRect(0, 0, canvas.width, canvas.height);
-          context.font = '40px Arial';
-          context.fillStyle = ' #0017ff';  // Texto
-          context.fillText('¡Apunta a los minions para que veas como funciona el cursor!', 50, 100);
-
-          // Usar el lienzo como textura
-          const texture = new THREE.CanvasTexture(canvas);
-          const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
-
-          // Crear el mesh (rectángulo con el texto)
-          const mesh = new THREE.Mesh(geometry, material);
-          mesh.position.set(0, 1, 0); // Ajusta la posición del plano
-          scene.add(mesh);
-        },
-      });
-*/
 
